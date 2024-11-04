9ae7b30152dac55eaf37ae1f88c5c3bf697091e9
tuyul <amirul@fazitsyssolutions.com.my>
2024-11-04 16:07:09 +0800

    Adding Banner

--- lazys3.py
+++ lazys3.py
@@ -2,24 +2,32 @@
 import os
 import configparser
 from tabulate import tabulate
-from colorama import init, Fore, Back, Style
+from colorama import init, Fore, Style
 import subprocess
-
+import threading
 import boto3
 import time
 import random
 
-print("""
-🟥⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
-🟥🟥⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
-🟥🟥🟥⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
-🟥🟥🟥🟥⬜⬜⬜⬜⬜⬜⬜⬜⬜
-🟥🟥🟥🟥🟥⬜⬜⬜⬜⬜⬜⬜⬜
-🟥🟥🟥🟥⬜⬜⬜⬜⬜⬜⬜⬜⬜
-🟥🟥🟥🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩
-🟥🟥🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩
-🟥🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩🟩 ® Free Palestine
-      
+# Initialize Colorama
+init(autoreset=True)
+
+class Color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[1;32m'
+    YELLOW = '\033[93m'
+    RED = '\033[1;31m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    BLINK = '\033[5m'
+    END = '\033[0m'
+
+colors = [Color.RED, Color.YELLOW, Color.GREEN, Color.CYAN, Color.BLUE]
+message = f"""
+{Color.BLINK}           
 ░▒▓█▓▒░       ░▒▓██████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓███████▓▒░▒▓███████▓▒░  
 ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░ 
 ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░    ░▒▓██▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░ 
@@ -28,37 +36,31 @@ print("""
 ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░         ░▒▓█▓▒░          ░▒▓█▓▒░      ░▒▓█▓▒░ 
 ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░  ░▒▓█▓▒░   ░▒▓███████▓▒░░▒▓███████▓▒░  
                                                                                 
-    """)
+®2024🇲🇾"""
 
+def display_banner():
+    while True:
+        for color in colors:
+            print(color + message + Color.END, end='\r')
+            time.sleep(1)
+            print(' ' * 80, end='\r')
 def load_aws_credentials():
-    """
-    Load AWS credentials from the default AWS credentials file (~/.aws/credentials).
-    """
     config = configparser.ConfigParser()
     config.read(os.path.expanduser("~/.aws/credentials"))
     profiles = config.sections()
     return profiles
 
 def login_to_s3(profile):
-    """
-    Log in to AWS S3 using credentials from the specified profile.
-    """
     session = boto3.Session(profile_name=profile)
     s3 = session.client('s3')
     return s3
 
 def list_buckets(s3):
-    """
-    List all buckets in the S3 account.
-    """
     response = s3.list_buckets()
     buckets = [bucket['Name'] for bucket in response['Buckets']]
     return buckets
 
 def list_objects_in_bucket(s3, bucket_name, prefix=None):
-    """
-    List objects in a specified S3 bucket.
-    """
     if prefix:
         response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
     else:
@@ -69,9 +71,6 @@ def list_objects_in_bucket(s3, bucket_name, prefix=None):
     return objects
 
 def list_directories_in_bucket(s3, bucket_name, prefix=None):
-    """
-    List directories in a specified S3 bucket.
-    """
     if prefix:
         response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix, Delimiter='/')
     else:
@@ -82,40 +81,21 @@ def list_directories_in_bucket(s3, bucket_name, prefix=None):
     return directories
 
 def download_object(s3, bucket_name, object_key, local_directory):
-    """
-    Download an object from a specified S3 bucket.
-    """
     local_filename = os.path.join(local_directory, os.path.basename(object_key))
     s3.download_file(bucket_name, object_key, local_filename)
     return local_filename
 
 def download_directory(s3, bucket_name, directory_prefix, local_directory):
-    """
-    Download all objects within a directory from a specified S3 bucket.
-    """
     objects = list_objects_in_bucket(s3, bucket_name, directory_prefix)
     for obj_key, _ in objects:
         download_object(s3, bucket_name, obj_key, local_directory)
 
 def download_all_objects(s3, bucket_name, local_directory):
-    """
-    Download all objects within a bucket to the specified local directory.
-    """
     objects = list_objects_in_bucket(s3, bucket_name)
     for obj_key, _ in objects:
         download_object(s3, bucket_name, obj_key, local_directory)
 
-def change_color():
-    """
-    Change the color of the table lines.
-    """
-    colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
-    return random.choice(colors)
-
 def prompt_keep_or_delete(image_path):
-    """
-    Prompt the user to keep or delete the downloaded image.
-    """
     while True:
         choice = input("Do you want to keep the downloaded image? (y/n): ").strip().lower()
         if choice == 'y':
@@ -129,9 +109,6 @@ def prompt_keep_or_delete(image_path):
             print("Invalid choice. Please enter 'y' or 'n'.")
 
 def navigate_directories(s3, bucket_name, base_directory=""):
-    """
-    Navigate directories within a bucket.
-    """
     current_directory = base_directory
     while True:
         directories = list_directories_in_bucket(s3, bucket_name, current_directory)
@@ -161,6 +138,10 @@ def navigate_directories(s3, bucket_name, base_directory=""):
             print("Invalid choice. Please try again.")
 
 def main():
+    banner_thread = threading.Thread(target=display_banner)
+    banner_thread.daemon = True
+    banner_thread.start()
+
     profiles = load_aws_credentials()
     
     if not profiles:
@@ -176,9 +157,6 @@ def main():
     
     s3 = login_to_s3(selected_profile)
 
-    color_timer = 5  # Change color every 5 seconds
-    start_time = time.time()
-
     while True:
         print("\nOptions:")
         print("1. List Buckets")
@@ -186,17 +164,10 @@ def main():
         print("3. Navigate Directories in a Bucket")
         print("4. Download Object")
         print("5. Download Directory")
-        print("6. Download All Objects in a Bucket")  # Added option
+        print("6. Download All Objects in a Bucket")
         print("7. Exit")
 
-        elapsed_time = time.time() - start_time
-        if elapsed_time > color_timer:
-            start_time = time.time()
-            color = change_color()
-        else:
-            color = Fore.RESET
-
-        choice = input(f"{color}Enter your choice: ")
+        choice = input("Enter your choice: ")
 
         if choice == '1':
             buckets = list_buckets(s3)
@@ -207,31 +178,26 @@ def main():
             print(tabulate(enumerate(objects, start=1), headers=["#", "Object Key", "Size"], tablefmt="fancy_grid"))
         elif choice == '3':
             bucket_name = input("Enter bucket name: ")
-            base_directory = input("Enter base directory (optional): ")
-            navigate_directories(s3, bucket_name, base_directory)
+            navigate_directories(s3, bucket_name)
         elif choice == '4':
             bucket_name = input("Enter bucket name: ")
-            object_key = input("Enter object key: ")
-            local_directory = input("Enter local directory to save: ")
-            local_filename = download_object(s3, bucket_name, object_key, local_directory)
-            print(f"Object '{object_key}' downloaded as '{local_filename}'")
+            object_key = input("Enter the object key to download: ")
+            local_directory = input("Enter the local directory to save the file: ")
+            download_object(s3, bucket_name, object_key, local_directory)
         elif choice == '5':
             bucket_name = input("Enter bucket name: ")
-            directory_prefix = input("Enter directory prefix: ")
-            local_directory = input("Enter local directory to save: ")
+            directory_prefix = input("Enter the directory prefix to download: ")
+            local_directory = input("Enter the local directory to save the files: ")
             download_directory(s3, bucket_name, directory_prefix, local_directory)
-            print(f"Directory '{directory_prefix}' downloaded to '{local_directory}'")
         elif choice == '6':
             bucket_name = input("Enter bucket name: ")
-            local_directory = input("Enter local directory to save: ")
+            local_directory = input("Enter the local directory to save the files: ")
             download_all_objects(s3, bucket_name, local_directory)
-            print(f"All objects in bucket '{bucket_name}' downloaded to '{local_directory}'")
         elif choice == '7':
-            print("Exiting...")
+            print("Exiting the program.")
             break
         else:
             print("Invalid choice. Please try again.")
 
 if __name__ == "__main__":
     main()
-

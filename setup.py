4a8f9a9a40f172092839a875310fc4eb22b951ad
tuyul <amirul@fazitsyssolutions.com.my>
2024-11-04 17:35:42 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lazys3',
+    version='1.2.1',
+    description='LAZYS3 is a powerful command-line tool designed for seamless interaction with Amazon S3 (Simple Storage Service). ',
+    author='stuxMY',
+    author_email='',
+    packages=find_packages(),
+    install_requires=[
+        boto3
+        colorama
+        tabulate
+
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  # Update as necessary
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',  # Update this to match your requirements
+)
